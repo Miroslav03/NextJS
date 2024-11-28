f5d0af0c57477f0fc22aae82d6929635b490ebb1
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-11-28 11:42:26 +0200

    Add meal details page

--- foodie-app/lib/meals.js
+++ foodie-app/lib/meals.js
@@ -6,3 +6,8 @@ export async function getMeals() {
     await new Promise((reslove) => setTimeout(reslove, 2000));
     return db.prepare("SELECT * FROM meals").all();
 }
+
+export async function getMeal(id) {
+    await new Promise((reslove) => setTimeout(reslove, 2000));
+    return db.prepare("SELECT * FROM meals WHERE slug = ?").get(id);
+}

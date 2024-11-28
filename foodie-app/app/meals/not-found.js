83f007766515b8ebb08289c377cb2eb7ae0d3394
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-11-28 11:48:30 +0200

    Add share page content

--- foodie-app/app/meals/not-found.js
+++ foodie-app/app/meals/not-found.js
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <main className="not-found">
-            <h1>Meal found</h1>
+            <h1>Meal not found</h1>
             <p>
                 Unfortunately, we could not find the requested page or meal data.
             </p>

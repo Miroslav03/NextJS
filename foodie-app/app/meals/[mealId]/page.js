6344adb048caa558881956253107bfe703452ac1
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-11-29 11:37:25 +0200

    Add metadata to other pages

--- foodie-app/app/meals/[mealId]/page.js
+++ foodie-app/app/meals/[mealId]/page.js
@@ -3,6 +3,19 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+    const meal = await getMeal(params.mealId);
+
+    if (!meal) {
+        notFound(); //Important either throwing error
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary,
+    };
+}
+
 export default async function MealDetailsPage({ params }) {
     const meal = await getMeal(params.mealId);
 
